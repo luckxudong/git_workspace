15d2c72ff6db193ae5440e1fe846205a5fc26640
donny <luckxudong@gmail.com>
2015-06-14 12:07:17 +0800

    simple layer and schedule

--- cocos_test/Classes/HelloWorldScene.cpp
+++ cocos_test/Classes/HelloWorldScene.cpp
@@ -2,31 +2,41 @@
 
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
-{
+Scene *HelloWorld::createScene() {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
+
     auto layer = HelloWorld::create();
 
-    // add layer as a child to scene
+    auto colorLayer = LayerColor::create(Color4B(255, 128, 128, 128));
+    auto director = Director::getInstance();
+    colorLayer->setContentSize(
+            Size(director->getVisibleSize().width / 4.0f, director->getVisibleSize().height / 4.0f)
+    );
+    colorLayer->setPosition(director->getVisibleSize().width/2, director->getVisibleSize().height/2);
+    scene->addChild(colorLayer, 0);
+
     scene->addChild(layer);
 
-    // return the scene
+    colorLayer->schedule(
+            [colorLayer](float delta) -> void {
+                colorLayer->setRotation(colorLayer->getRotation() + 1);
+            },
+            0.01,
+            "custom_schedule"
+    );
+
     return scene;
 }
 
 // on "init" you need to initialize your instance
-bool HelloWorld::init()
-{
+bool HelloWorld::init() {
     //////////////////////////////
     // 1. super init first
-    if ( !Layer::init() )
-    {
+    if (!Layer::init()) {
         return false;
     }
-    
+
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
@@ -36,12 +46,12 @@ bool HelloWorld::init()
 
     // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
+            "CloseNormal.png",
+            "CloseSelected.png",
+            CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+
+    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width / 2,
+            origin.y + closeItem->getContentSize().height / 2));
 
     // create menu, it's an autorelease object
     auto menu = Menu::create(closeItem, NULL);
@@ -53,31 +63,30 @@ bool HelloWorld::init()
 
     // add a label shows "Hello World"
     // create and initialize a label
-    
+
     auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
+
     // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
+    label->setPosition(Vec2(origin.x + visibleSize.width / 2,
+            origin.y + visibleSize.height - label->getContentSize().height));
 
     // add the label as a child to this layer
     this->addChild(label, 1);
 
     // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+    auto sprite = Sprite::create("tab_me_checked.png");
 
     // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    sprite->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
 
     // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
+    this->addChild(sprite);
+
     return true;
 }
 
 
-void HelloWorld::menuCloseCallback(Ref* pSender)
-{
+void HelloWorld::menuCloseCallback(Ref *pSender) {
     Director::getInstance()->end();
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
